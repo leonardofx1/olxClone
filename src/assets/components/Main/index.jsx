dfb178ff752e828b7473c0b4777519e597694369
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-05-03 12:53:31 -0300

    'links'

--- src/assets/components/Main/index.jsx
+++ src/assets/components/Main/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import {BsSearch } from 'react-icons/bs';
+import { TfiBasketball}1from "react-icons/tfi";
+
+
 
 import * as C from './mainStyled'
+import Links from './subComponents/LinksMain';
 
 const Main = () => {    
 
     return (
            <C.Main>
             <C.Section>
-               <C.Form><input type='text'  />
+               <C.Form>
+                <input type='text'   placeholder='Buscar'/>
                <button><BsSearch/></button>
                </C.Form>
-               <p>hahahaha</p>
+                <C.Nav>
+                    <ul>
+                        <Links icon={<TfiBasketball/>} desc={'Esportes e Lazer'} />
+                    </ul>
+                    </C.Nav>
             </C.Section>
            </C.Main>
     )
